1936d1a8a10b14333d0974b45d0df72668952a27
Rodney <netmonger@users.noreply.github.com>
2014-11-18 18:36:58 -0600

    Created mp4_3.py

--- class4/mp4_3.py
+++ class4/mp4_3.py
@@ -3,4 +3,12 @@ uptime2 = '3750RJ uptime is 1 hour, 29 minutes'
 uptime3 = 'CATS3560 uptime is 8 weeks, 4 days, 18 hours, 16 minutes'
 uptime4 = 'rtr1 uptime is 5 years, 18 weeks, 8 hours, 23 minutes'
 
+list = [uptime1, uptime2, uptime3, uptime4]
+output = {}
 
+for string in list:
+	device_name = string.split()[0]
+
+	output[device_name] = 1
+
+print output 
