bc45a644f9cb11deb1b28fba6e18dddb8229a4dd
Rodney <netmonger@users.noreply.github.com>
2014-11-19 19:05:46 -0600

    Added mp4_3.py

--- class4/mp4_3.py
+++ class4/mp4_3.py
@@ -9,6 +9,21 @@ output = {}
 for string in list:
 	device_name = string.split()[0]
 
-	output[device_name] = 1
+	parse_time = ((string.split("is "))[1].split(", "))
+	print parse_time
 
-print output 
+	seconds = 0
+	for time_unit in parse_time:
+		if "years" in time_unit:
+			years = time_unit.split()[0]
+			seconds = int(years) * 31560000000
+		if "weeks" in time_unit:
+			weeks = time_unit.split()[0]
+			seconds = seconds + (int(weeks) * 604800)
+		if "days" in time_unit:
+			days = time_unit.split()[0]
+			seconds = seconds + (int(days) * 86400)
+	print seconds
+
+#	output[device_name] = parse_time
+
