f51c14682acbb74aa910c1631057e28beb230619
Rodney <netmonger@users.noreply.github.com>
2014-12-09 06:29:28 -0600

    Adding CDP info

--- class5/mp5_1.py
+++ class5/mp5_1.py
@@ -1,3 +1,29 @@
 import pprint
 
+sw1_show_cdp_neighbors = '''
+SW1>show cdp neighbors
+Capability Codes: R - Router, T - Trans Bridge, B - Source Route Bridge
+                                 S - Switch, H - Host, I - IGMP, r - Repeater, P - Phone
+Device ID            Local Intrfce        Holdtme        Capability        Platform    Port ID
+R1                    Fas 0/11              153            R S I           881          Fas 1
+R2                    Fas 0/12              123            R S I           881          Fas 1
+R3                    Fas 0/13              129            R S I           881          Fas 1
+R4                    Fas 0/14              173            R S I           881          Fas 1
+R5                    Fas 0/15              144            R S I           881          Fas 1
+
+
+'''
+
+
+sw1_show_cdp_neighbors_detail = '''
+
+SW1> show cdp neighbors detail
+--------------------------
+Device ID: R1
+Entry address(es):
+	   IP address: 10.1.1.1
+Platform: Cisco 881, Capabilities: Router Switch IGMP
+Interface: FastEthernet0/11, Port ID (outgoing port): FastEthernet1
+Holdtime: 153 sec
+
 
