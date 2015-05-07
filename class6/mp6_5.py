2631b1770570c338e2fb716d96e3b99fe57b9764
Rodney <netmonger@users.noreply.github.com>
2015-05-07 17:29:17 -0500

    Finished mp6_5.py

--- class6/mp6_5.py
+++ class6/mp6_5.py
@@ -7,8 +7,8 @@ from mp6_4 import dec_to_bin
 
 ip_addr = raw_input("Please enter an IP address: ")
 
-if check_ip(ip_addr) is False:
-	print "Bad IP address."
+if check_ip(ip_addr) is True:
+	print dec_to_bin(ip_addr)
 	sys.exit()
 else:
-
+	print "Bad IP address"
